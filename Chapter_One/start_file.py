4bf095ebab10f507f6e212b033bcbbbfcd0fa83a
Sachin Kafle <46559660+sachinkafle@users.noreply.github.com>
2019-03-28 12:48:05 +0545

    Add files via upload

---
+++ Chapter_One/start_file.py
@@ -0,0 +1,5 @@
+print(4 + 5) #prints result of addition
+print(4 + 11)
+print("Hello world") #prints string hello world
+print(4 * 5) #prints result of multiplication 20
+print(4 / 5)#prints result of division 0.8
