66580e0a8faad1283ef391a965e352523bd1308c
Sachin Kafle <46559660+sachinkafle@users.noreply.github.com>
2019-09-19 16:42:30 -0700

    Add files via upload

---
+++ Chapter_11/SnakeRect.py
@@ -0,0 +1,17 @@
+  """
+Code before making snake head image
+
+ game.draw.rect(DisplayScreen, color_red, [XpositionApple, YpositionApple, Width_Apple, Width_Apple])
+        game.draw.rect(DisplayScreen, color_black, [change_x,change_y,pixel_size, pixel_size])
+        game.display.update()
+
+        objectClock.tick(FPS)
+        
+    game.quit()
+    quit()
+
+MainLoopForGame()
+
+
+
+"""
