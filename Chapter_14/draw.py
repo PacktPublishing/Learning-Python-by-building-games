119b636e7e0897d73589eb515146eed9b5ee791a
Sachin Kafle <46559660+sachinkafle@users.noreply.github.com>
2019-09-19 14:05:11 -0700

    Update draw.py

--- Chapter_14/draw.py
+++ Chapter_14/draw.py
@@ -1,16 +1,30 @@
+from OpenGLContext import testingcontext
+BaseContext = testingcontext.getInteractive()
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from pygame.locals import *
-window_screen = pygame.display.set_mode((640, 480), HWSURFACE|OPENGL|DOUBLEBUF)
 
-#Draw a geometry for the scene
-def Draw():
-       #translation (moving) about 6 unit into the screen and 1.5 unit to left
-       glTranslatef(-1.5,0.0,-6.0)
-       glBegin(GL_TRIANGLES) #GL_TRIANGLE is constant for TRIANGLES        
-       glVertex3f( 0.0, 1.0, 0.0) #first vertex       
-       glVertex3f(-1.0, -1.0, 0.0)  #second vertex   
-       glVertex3f( 1.0, -1.0, 0.0) #third vertex     
-       glEnd()
+"""
+we have to install OpenGLContext module which will store all the states that 
+are associated with the instances of OpenGL. 
+If the context is destroyed then eventually OpenGL will be destroyed.
 
-Draw()
+"""
+
+
+class TriangleContext( BaseContext ):
+
+    initialPosition = (0, 0, 0)  
+    
+    #define render
+    def Render(self, mode):
+        
+        """ Render triangle"""
+        glDisable(GL_CULL_FACE)
+        glTranslatef(-1.5, 0.0, -6.0)
+        glBegin(GL_TRIANGLES)
+        glVertex3f(0.0, 1.0, 0.0)
+        glVertex3f(-1.0, -1.0, 0.0)
+        glVertex3f(1.0, -1.0, 0.0)
+        glEnd()
+
+if __name__ == "__main__":
+    TriangleContext.ContextMainLoop()
